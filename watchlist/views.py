501018f900ee11938986995d35ba30abfb6a19ce
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-23 15:57:57 +0600

    manual movie list api

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Movie
 
-# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data) # py dict to json response
