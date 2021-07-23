745e31934b52fbdc0a4e5858fc3e03e94adcd43a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-23 15:38:45 +0600

    movie model created

--- watchlist/models.py
+++ watchlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """ Movie model """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.name
