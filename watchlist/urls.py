6574d1de1d97bbf1cd63fcfecb3d6b5d54ce70d1
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-23 16:04:31 +0600

    movie detail fbv api

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import movie_list
+from .views import movie_list, movie_details
 
 urlpatterns = [
     path('list/', movie_list, name='movie-list'),
+    path('<int:pk>/detail/', movie_details, name='movie-details'),
 ]
--- watchlist/views.py
+++ watchlist/views.py
@@ -3,8 +3,26 @@ from django.http import JsonResponse
 from .models import Movie
 
 def movie_list(request):
+    """ movie list api """
     movies = Movie.objects.all()
     data = {
         'movies': list(movies.values())
     }
     return JsonResponse(data) # py dict to json response
+
+
+def movie_details(request, pk):
+    """ Movie details api """
+    try:
+        movie = Movie.objects.get(pk=pk)
+        data = {
+            'name': movie.name,
+            'description': movie.description,
+            'active': movie.active,
+        }
+    except Movie.DoesNotExist:
+        data = {
+            'error': 'Data not from'
+        }
+
+    return JsonResponse(data)
