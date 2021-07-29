a3b7a63bb9e4ebcbb90e8cd0999f45da0a22280c
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-29 22:05:28 +0600

    custom permissions updated

--- watchlist/api/permissions.py
+++ watchlist/api/permissions.py
@@ -4,14 +4,19 @@ class AdminOrReadOnly(permissions.IsAdminUser):
     message = 'Allowed access to only admin users.'
 
     def has_permission(self, request, view):
-        return  request.method == 'GET' or bool(request.user and request.user.is_staff)
+        if request.method in permissions.SAFE_METHODS: # SAFE_METHODS = get
+            # Check permissions for read-only request
+            return True
+        else: # post, put
+            # Check permissions for write request
+            return  request.method == 'GET' or bool(request.user and request.user.is_staff)
 
 
 class ReviewUserOrReadOnly(permissions.BasePermission):
     message = 'Allowed access to only review owner.'
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS: # SAFE_METHODS = get
             # Check permissions for read-only request
             return True
         else:
