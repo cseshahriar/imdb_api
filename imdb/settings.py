e02190525bf92cf30a177f42c90c3f3b10ab0887
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-29 21:25:28 +0600

    global permissions

--- imdb/settings.py
+++ imdb/settings.py
@@ -129,3 +129,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# global settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', # AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
+    ]
+}
