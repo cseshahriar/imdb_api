43197103e22650a5f5bb7c13124d08024cb1f418
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-30 15:01:25 +0600

    api logout

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
+from .views import logout
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token, name='api-login'),
+    path('api-logout/', logout, name='api-logout'),
 ]
