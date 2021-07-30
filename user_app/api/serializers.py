7a4beac08a638d97d495311c34b361255132a89a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-30 17:10:20 +0600

    after registration return token

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer
 
 @api_view(['POST'])
@@ -8,12 +9,22 @@ def registration(request):
     """ api register views """
     if request.method == 'POST':
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+        data = {}
 
+        if serializer.is_valid():
+            user = serializer.save()
+
+            data['response'] = "Registration Successfull!"
+            data['username'] = user.username
+            data['email'] = user.email
+            
+            token = Token.objects.get(user=user).key
+            data['token'] = token
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
 
 @api_view(['POST'])
 def logout(request):
