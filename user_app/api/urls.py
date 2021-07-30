478b90f16f90270ea4d516328f204a47b6a689d7
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-07-30 16:20:57 +0600

    api registration

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
-from .views import logout
+from .views import logout, registration
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token, name='api-login'),
+    path('api-register/', registration, name='api-register'),
     path('api-logout/', logout, name='api-logout'),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,9 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import RegisterSerializer
+
+@api_view(['POST'])
+def registration(request):
+    """ api register views """
+    if request.method == 'POST':
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 @api_view(['POST'])
 def logout(request):
+    """ api logout views """
+
     if request.method == 'POST':
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
